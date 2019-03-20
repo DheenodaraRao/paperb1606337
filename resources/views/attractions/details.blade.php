308409290394654881b7eab4096dcc8ee17a5b49
Dheenodara <dheenodararao@gmail.com>
2019-03-20 15:46:37 +0800

    Adding Details Text

--- resources/views/attractions/details.blade.php
+++ resources/views/attractions/details.blade.php
@@ -3,7 +3,6 @@
 @section('content')
 <div class="container">
     <h2>Details</h2>
-    <h3>Click for More Details</h3>
     <div id="content-details">
         
     </div>
--- resources/views/attractions/index.blade.php
+++ resources/views/attractions/index.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 <div class="container">
     <h2>All Attraction Places</h2>
+    <h3>Click for More Details</h3>
     <div id="content-attractions">
         
     </div>
